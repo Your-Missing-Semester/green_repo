2da2cf229c6c70f722ec5835ea5f7890c1970457
Fahad Khan <121646456+fahadakhan2@users.noreply.github.com>
2024-05-28 09:16:06 -0500

    T8: Create Footer (#2)
    * footer before styling
    
    * added styling to footer component
    
    * changed file extension from js to jsx
    
    * adressing PR review comments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Footer } from './components/Footer/Footer';
+import { BrowserRouter } from 'react-router-dom';
 import { NavBar } from './components/NavBar/NavBar';
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,51 @@
+import { Link } from 'react-router-dom';
+import styles from './Footer.module.css';
+import logoPlaceHolder from '../../assets/logoPlaceHolder.png';
+
+export const Footer = () => {
+  return (
+    <div className="container">
+      <footer>
+        <div>
+          <img src={logoPlaceHolder} alt="logo" />
+        </div>
+        <div className={styles.footerInfoContainer}>
+          <div>
+            <Link
+              to="/terms-of-service"
+              className={styles.footerText}
+              id={styles.tos}
+            >
+              Terms of Service
+            </Link>
+            <span className={styles.verticalBar}>|</span>
+            <span>
+              <Link
+                to="/privacy-policy"
+                className={styles.footerText}
+                id={styles.pp}
+              >
+                Privacy Policy
+              </Link>
+            </span>
+            <p className={styles.footerText}>
+              Copyright &copy; 2024 WayPoints | All Rights Reserved
+            </p>
+          </div>
+          <div>
+            <Link
+              to="/contact-us"
+              className={styles.footerText}
+              id={styles.contact}
+            >
+              Contact Us
+            </Link>
+            <p className={styles.footerText}>
+              Business Inquiries | help@waypoints.me
+            </p>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
