6ac9aba258a1ec69331fb26b4c85465aba3b05e9
brigittewylee <brigitte.leewy@gmail.com>
2024-06-02 01:21:38 -0400

    signup and login page

---
+++ frontend/src/components/LogIn/login.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './login.css';
+import gmail_icon from './gmail_icon.png';
+import fb_icon from './fb_icon.png';
+import apple_icon from './apple_icon.png';
+import twtx_icon from './twtx_icon.webp';
+
+
+export function LogIn() {
+    return (
+        <div className="loginGroup">
+            <h1>SIGN IN</h1>
+            <form>
+                <label>Email</label>
+                    <input type="text" />
+                <label>Password</label>
+                    <input type="text" />
+                    <p className='passReset'>Forgot Password?</p>
+                <button className='loginButton'>LOGIN</button>
+                <button className='signupButton'>SIGN UP</button>
+            </form>
+            <p className='altLoginSubtext'><span>LOGIN WITH</span></p>
+            <table>
+                <tr className='icons'>
+                   <img className='gmailLogo' src={gmail_icon} alt='gmail'></img>
+                   <img className='fbLogo' src={fb_icon} alt='fb'></img>
+                   <img className='twtLogo' src={twtx_icon} alt='twitter'></img>
+                   <img className='appleLogo' src={apple_icon} alt='apple'></img>
+                </tr>
+            </table>
+        </div>        
+    )
+}
---
+++ frontend/src/components/SignUp/signup.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './signup.css';
+import gmail_icon from './gmail_icon.png';
+import fb_icon from './fb_icon.png';
+import apple_icon from './apple_icon.png';
+import twtx_icon from './twtx_icon.webp';
+
+
+export function SignUp() {
+    return (
+        <div className="signup-group">
+            <h1>SIGN UP</h1>
+            <p className='createAccount-subtext'>create account</p>
+            <form>
+                <label>Email</label>
+                    <input type="text" />
+                <label>Password</label>
+                    <input type="text" />
+                <label>Confirm Password</label>
+                    <input type="text" /><br></br>
+                <button className='signup-button'>SIGN UP</button>
+            </form>
+            <p className='login-subtext'>Already have an account? <span className='login-button'>Login here</span></p>
+            <p className='altLogin-subtext'><span>OR SIGN UP WITH</span></p>
+            <table>
+                <tr className='icons'>
+                   <img className='gmailLogo' src={gmail_icon} alt='gmail'></img>
+                   <img className='fbLogo' src={fb_icon} alt='fb'></img>
+                   <img className='twtLogo' src={twtx_icon} alt='twitter'></img>
+                   <img className='appleLogo' src={apple_icon} alt='apple'></img>
+                </tr>
+            </table>
+        </div>        
+    )
+}
