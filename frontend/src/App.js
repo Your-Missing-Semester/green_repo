96fe4780baceaf66b61e8284abad1a866c045337
brigittewylee <brigitte.leewy@gmail.com>
2024-06-15 20:18:30 -0400

    all changes to login and signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,24 @@ import './App.css';
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import { LogIn } from './components/LogIn/login';
 import { SignUp } from './components/SignUp/signup';
-
-
+import { landingPage } from './components/landingBodyPage/landingPage';
 function App() {
   return (
-    <div className="App">
-      <Router>
-          <Routes>
-              <Route exact path="/" element={<h1>Home Page</h1>} />
-              <Route exact path="/SignUp" element={<SignUp />} />
-              <Route exact path="/LogIn" element={<LogIn />} />
-          </Routes>
-      </Router>
-    </div>
+    <landingPage></landingPage>
+    // <div className="App">
+    //   <Router>
+    //       <Routes>
+    //           <Route exact path="/" element={<h1>Home Page</h1>} />
+    //           <Route exact path="/SignUp" element={<SignUp />} />
+    //           <Route exact path="/LogIn" element={<LogIn />} />
+    //           <Route exact path="/landingBodyPage" element={<landingBodyPage />} />
+    //       </Routes>
+    //       <ul>
+    //         <Link to="/landingBodyPage">g</Link>
+    //       </ul>
+    //   </Router>
+    // </div>
+
   );
 }
 
