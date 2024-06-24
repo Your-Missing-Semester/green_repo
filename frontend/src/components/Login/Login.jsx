dc5466307c406203ef517e783ec7a0fbb8702ee1
Fahad Khan <kanlifahad@gmail.com>
2024-06-23 21:44:33 -0500

    Outline jsx for Login component

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,3 +1,22 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 export default function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleLoginFormSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <div className="flex h-screen font-sans">
+      <div className="w-7/12 flex justify-center items-center">
+        <form
+          className="flex flex-col w-9/12"
+          onSubmit={handleLoginFormSubmit}
+        ></form>
+      </div>
+    </div>
+  );
 }
