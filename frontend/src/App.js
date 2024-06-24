8522d27c7384f23c0a7be6debf78ee9edba52318
Fahad Khan <kanlifahad@gmail.com>
2024-06-23 21:21:50 -0500

    Initial setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
 import SignUp from './pages/SignUp/SignUp';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/Login/Login.jsx
@@ -0,0 +1,5 @@
+import LoginForm from '../../components/Login/Login';
+
+export default function Login() {
+  return <LoginForm />;
+}
