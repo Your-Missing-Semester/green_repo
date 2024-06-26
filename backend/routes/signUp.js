ff09a89b2aff949c29377d88415d20b24adafce6
Fahad Khan <kanlifahad@gmail.com>
2024-06-25 22:31:02 -0500

    Beefing up the schema to do the same validation checks that the frontend is doing for email and password.

--- backend/routes/signUp.js
+++ backend/routes/signUp.js
@@ -7,15 +7,24 @@ const signUpRouter = Router();
 const signUpSchema = {
   firstName: {
     isString: true,
+    errorMessage: 'First name must be a string',
   },
   lastName: {
     isString: true,
+    errorMessage: 'Last name must be a string',
   },
   email: {
-    isString: true,
+    isEmail: true,
+    errorMessage: 'Invalid email format',
   },
   password: {
     isString: true,
+    errorMessage: 'Password must be a string',
+    matches: {
+      options: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+      errorMessage:
+        'Password must contain at least one number, lowercase, uppercase letter and a special character, and be at least 8 characters long',
+    },
   },
 };
 
